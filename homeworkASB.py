beee03a70f6293d1d90bf605eb6184f914e09909
jonnymoretti <97030693+jonnymoretti@users.noreply.github.com>
2023-07-01 12:04:53 +0100

    homeworkASB.py

--- homeworkASB.py
+++ homeworkASB.py
@@ -1,21 +1,25 @@
 from Bio import Entrez
 import sys
 
+# Set your email address for Entrez queries
 Entrez.email = "youremail@email.com"
 
-def Esearch(database,searchterm):
+def Search_databases(database,searchterm):
+    # Perform the search in the specified database with the search term
     handle = Entrez.esearch(db=database, term=searchterm,usehistory='y')
     record = Entrez.read(handle)
     return record
 
 def History(record):
-    
+    # Extract the QueryKey and WebEnv from the search record
     querykey = record["QueryKey"]
     webenv = record["WebEnv"]
-    y = querykey + "\n" + webenv
-    return y
+    # Concatenate the QueryKey and WebEnv as a string
+    history_info = querykey + "\n" + webenv
+    return history_info
     
-def Efetch(database,record):
+def Fetch_sequences(database,record):
+    # Retrieve the sequences using the QueryKey and WebEnv
     querykey = record["QueryKey"]
     webenv = record["WebEnv"]
     fhandle = Entrez.efetch(db=database,query_key=querykey,WebEnv=webenv,rettype = "fasta")
@@ -24,7 +28,11 @@ def Efetch(database,record):
     return sequences
 
 if __name__ == '__main__':
-    record = Esearch(sys.argv[1], sys.argv[2])
+    # Perform the search using the arguments provided in the command line
+    record = Search_databases(sys.argv[1], sys.argv[2])
+    # Print the search record
     print(record)
+    # Extract and print the search history information
     print(History(record))
-    print(Efetch(sys.argv[1],record))
+    # Fetch the sequences and print the result
+    print(Fetch_sequences(sys.argv[1],record))
